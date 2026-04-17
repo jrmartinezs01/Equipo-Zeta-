bec2abf76527661e1c3ae1104a7cc5acade725f9
Juan Ramón Martínez Sánchez <jrmartinezs01@educarex.es>
2026-04-17 20:01:57 +0200

    Actualización de backlog y mejoras visuales en listar.js

--- fuentes/js/listar.js
+++ fuentes/js/listar.js
@@ -9,7 +9,10 @@ function leerUsuario() {
     const li = document.createElement("li");
     li.innerHTML = `
       <span><strong>${usuario.usuario}</strong> - ${usuario.robot}</span>
-      <button class="btn-delete" onclick="borrarUsuario(${usuario.id})">Eliminar</button>
+      <div class="botones-lista">
+        <button class="btn-edit" onclick="prepararEdicion(${usuario.id})">Editar</button>
+        <button class="btn-delete" onclick="borrarUsuario(${usuario.id})">Eliminar</button>
+      </div>
     `;
     lista.appendChild(li);
   });
