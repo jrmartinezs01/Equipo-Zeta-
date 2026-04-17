0ddab5e910da91419dd120e8233d0292dbbf0a15
Alberto clase <arodriguezp29@educarex.es>
2026-04-17 20:09:43 +0200

    Inicio de la funcion de actualizar

--- fuentes/js/controladores/Update.js
+++ fuentes/js/controladores/Update.js
@@ -1,12 +1,5 @@
 function Actualizarusuario(id){
-const vistaUpdate=document.querySelector('#vistaUpdate')
-const botonUpdate=document.querySelector('.Actualizar')
-botonUpdate.addEventListener('click', verVistaUpdate)
-function verVistaUpdate(){
-	ocultarVistas()
-	vistaUpdate.classList.add('activa')
-	console.log('a')
-}
+	
 }
 
 
