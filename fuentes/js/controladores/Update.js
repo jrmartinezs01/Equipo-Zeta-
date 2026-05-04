7e5b32f975d037884f405fcbba593f9e289017f0
Alberto Rodriguez <arodriguezp29@educarex.es>
2026-05-04 21:25:35 +0200

    terminado el update

--- fuentes/js/controladores/Update.js
+++ fuentes/js/controladores/Update.js
@@ -1,21 +1,44 @@
 function Actualizarusuario(id){
-const lista = document.getElementById("listaUsuarios");
-  if (!lista) return;
+const vistaUpdate=document.querySelector('#vistaUpdate')
+const botonUpdate=document.querySelector('.Actualizar')
+botonUpdate.addEventListener('click', verVistaUpdate)
+function verVistaUpdate(){
+	const usuario = usuarios.find(u => u.id === id);
+	ocultarVistas()
+	vistaUpdate.classList.add('activa')
+	const lista = document.getElementById("listaUpdate");
+	if (!lista) return;
+    lista.innerHTML = `
+  Usuario: <input id="editUsuario" value="${usuario.usuario}"><br>
+  Contraseña: <input id="editPass" value="${usuario.contraseña}"><br>
+  Nacimiento: <input id="editNacimiento" value="${usuario.nacimiento}"><br>
+  Robot: <input id="editRobot" value="${usuario.robot}"><br>
+  Modo: <input id="editModo" value="${usuario.modo}"><br>
+  Mejoras: <input id="editMejoras" value="${usuario.mejoras.join(", ")}"><br>
+  Mision: <input id="editMision" value="${usuario.mision}"><br>
+  <button id="guardarCambios">Guardar</button>
+	`;
+document.getElementById("guardarCambios").addEventListener("click", () => {
+  guardarCambios(usuario.id);
+});
+		function guardarCambios(id) {
+  const usuario = usuarios.find(u => u.id === id);
 
-  lista.innerHTML = "";
+  if (!usuario) return;
 
-  usuarios.forEach(usuario => {
-    const li = document.createElement("li");
-    li.innerHTML = `
-      <span><strong>${usuario.usuario}</strong> - ${usuario.robot}</span>
-      <button class="btn-delete" onclick="borrarUsuario(${usuario.id})">Eliminar</button>
-      <button class="Actualizar" onclick="Actualizarusuario(${usuario.id})">Actualizar</button>
-      `;
-    lista.appendChild(li);
-  });
-	console.log('e')
+  usuario.usuario = document.getElementById("editUsuario").value;
+  usuario.contraseña = document.getElementById("editPass").value;
+  usuario.nacimiento = document.getElementById("editNacimiento").value;
+  usuario.robot = document.getElementById("editRobot").value;
+  usuario.modo = document.getElementById("editModo").value;
+  usuario.mejoras = document.getElementById("editMejoras").value.split(", ");
+  usuario.mision = document.getElementById("editMision").value;
+
+  alert("Usuario actualizado correctamente");
+
+  console.log(usuarios); // para comprobar
 }
-
-
+}
+}
 
 
